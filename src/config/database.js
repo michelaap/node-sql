187b8be72f303425ad580e575346308a53aa6af8
Michel Pereira <michel.augusto@pg.inatel.br>
2019-10-16 21:35:24 -0300

    first commit

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'postgres',
+  database: 'nodesql',
+  define: {
+    timestamps: true,
+    underscored: true,
+  },
+};
